791e65015b2754d0bad5b8d6a55d127466c7b150
isergioam <isergioam@gmail.com>
2026-04-13 10:14:06 +0200

    Creación de proyecto JAVA + ANGULAR

---
+++ arenaxp/src/main/java/com/paco/arenaxp/ArenaxpApplication.java
@@ -0,0 +1,13 @@
+package com.paco.arenaxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaxpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaxpApplication.class, args);
+	}
+
+}
---
+++ arenaxp/src/test/java/com/paco/arenaxp/ArenaxpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paco.arenaxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaxpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
